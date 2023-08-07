957a2a05e75b7f3b36343be11d054f6b3ea97a98
karan147369 <103363950+karan147369@users.noreply.github.com>
2023-08-08 00:24:29 +0530

    route changes

--- route/index.js
+++ route/index.js
@@ -13,7 +13,7 @@ var interviewList = [];
 const checkAuthentication = async (req, res, next) => {
   List = await getStudentList();
   if (req.isAuthenticated()) return next();
-  return res.render("home.ejs");
+  return res.redirect("/");
 };
 const checkNotAuthenticated = async (req, res, next) => {
   List = await getStudentList();
